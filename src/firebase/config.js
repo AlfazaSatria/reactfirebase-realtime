49c740db108a6370c1125377d924af3c2a9c85ec
Alfaza Satria <alfazasatria8@gmail.com>
2020-05-01 05:34:34 +0700

    Add config database

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdXX9B2c06LfZ7K4MRJMXmo5AsJk9peQM",
+    authDomain: "loginreact-39a75.firebaseapp.com",
+    databaseURL: "https://loginreact-39a75.firebaseio.com",
+    projectId: "loginreact-39a75",
+    storageBucket: "loginreact-39a75.appspot.com",
+    messagingSenderId: "63670430494",
+    appId: "1:63670430494:web:4e5afdc9384649767cf70f"
+  };
